6087a51e30cbe4a0e1b9b1df78aef8f4870fc8f0
MochHamimuddin <mochhamimuddin@gmail.com>
2023-04-12 07:19:02 +0700

    Tugas MSIB

--- assets/layout/content.php
+++ assets/layout/content.php
@@ -2,7 +2,7 @@
     require_once 'assets/layout/content/about.php';
     require_once 'assets/layout/content/home.php';
     require_once 'assets/layout/content/porto.php';
-    require_once 'assets/layout/content/study.php';
-    require_once 'assets/layout/content/tasks.php';
+    require_once 'assets/layout/content/resume.php';
+    require_once 'assets/layout/content/skills.php';
     require_once 'assets/layout/content/contact.php';
 ?>
